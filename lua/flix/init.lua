fcc86a1a5172351857b448da3f406f5713c7510e
stormckey <chenhaogao123@gmail.com>
2025-02-13 15:33:22 +0100

    init

---
+++ lua/flix/init.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+function M.setup(user_config)
+	local config = vim.tbl_deep_extend("force", {
+		flix_jar_path = "flix.jar", -- Default path to flix.jar
+		enable_codelens = true,
+	}, user_config or {})
+
+	local lspconfig = require("lspconfig")
+	local configs = require("lspconfig.configs")
+	local start_cmd = { "java", "-jar", config.flix_jar_path, "lsp" }
+
+	vim.filetype.add({ extension = { flix = "flix" } })
+
+	-- Register the Flix LSP configuration
+	if not configs.flix then
+		configs.flix = {
+			default_config = {
+				cmd = start_cmd,
+				filetypes = { "flix" },
+				root_dir = function(fname)
+					local root_dir = vim.fs.dirname(vim.fs.find("flix.toml", { path = fname, upward = true })[1])
+						or vim.loop.cwd()
+					local flix_jar_path = root_dir .. "/" .. config.flix_jar_path
+					if vim.loop.fs_stat(flix_jar_path) == nil then
+						print(
+							"Failed to start the LSP server: flix.jar not found in project root (" .. root_dir .. ")!"
+						)
+						return nil
+					end
+					return root_dir
+				end,
+				settings = {},
+			},
+		}
+	end
+
+	-- Setup the Flix server
+	lspconfig.flix.setup({
+		capabilities = vim.lsp.protocol.make_client_capabilities(),
+		on_attach = function(client, bufnr)
+			if config.enable_codelens then
+				-- Register the Flix run command
+				client.commands["flix.runMain"] = function(_, _)
+					vim.cmd("split | terminal java -jar " .. config.flix_jar_path .. " run")
+				end
+			end
+		end,
+		flags = {},
+	})
+end
+
+return M
