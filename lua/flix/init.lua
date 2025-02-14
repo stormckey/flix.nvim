5e810fbf1e57b6f206f79deb116a87474303351a
stormckey <chenhaogao123@gmail.com>
2025-02-14 13:24:57 +0100

    add empty line

--- lua/flix/init.lua
+++ lua/flix/init.lua
@@ -25,6 +25,7 @@ function M.setup(user_config)
 					local flix_jar_path = vim.fs.joinpath(root_dir, config.flix_jar_path)
 					-- Make sure flix.jar is found in the root directory, otherwise return nil to prevent the LSP server from starting
 					if vim.loop.fs_stat(flix_jar_path) == nil then
+						print("")
 						print("Failed to start the LSP server: flix.jar not found in project root (" .. root_dir .. ")!")
 						return nil
 					end
